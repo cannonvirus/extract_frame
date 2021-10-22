4e93d3aad4c51de53067fd11623883beee13d224
cannonvirus <cannonvirus@intflow.ai>
2021-10-22 18:02:47 +0900

    add module

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import cv2
 import yaml
+import albumentations as A
 
 def main(config):
     print("----------------------------")
@@ -9,6 +10,11 @@ def main(config):
     print("----------------------------")
 
     cap = cv2.VideoCapture(config['video_path'])
+    first_name = os.path.splitext(os.path.basename(config['video_path']))[0]
+
+    # if not os.path.isdir(os.path.join(config['out_path'], first_name)):
+    #     os.mkdir(os.path.join(config['out_path'], first_name))
+
     frame_count = 0
 
     while(1):
@@ -17,9 +23,22 @@ def main(config):
         if not ret:
             break
         
+        # X : 240 ~ 1680
         if frame_count % config['save_frame_count'] == 0:
+
+            if not os.path.isdir(os.path.join(config['out_path'], "1_" + first_name + "_" +  str(frame_count).zfill(5))):
+                os.mkdir(os.path.join(config['out_path'], "1_" + first_name + "_" +  str(frame_count).zfill(5)))
+
+            clean_img = frame[:,240:1680,:]
+            for id in range(10):
+                transform = A.Compose([A.RandomSizedCrop(min_max_height=[500,1000], height=config['out_img_size'], width=config['out_img_size'], p=1)])
+                transformed = transform(image=clean_img)
+                cv2.imwrite(os.path.join( config['out_path'], "1_" + first_name + "_" +  str(frame_count).zfill(5), "{}.jpg".format(str(id).zfill(3))), transformed["image"])
+
+            frame_re = cv2.resize(frame, (config['out_img_size'], config['out_img_size']))
+            cv2.imwrite(os.path.join(config['out_path'], "1_" + first_name + "_" +  str(frame_count).zfill(5), "origin.jpg"), frame_re)
+
             print("frame : {}".format(frame_count))
-            cv2.imwrite(  os.path.join(config['save_img_path'], "{}.jpg".format(str(frame_count).zfill(5))), frame )
 
         frame_count += 1
 
